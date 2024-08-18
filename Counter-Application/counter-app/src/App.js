8a0e8d80da4651ce8a63f2ded51421210f5aa61e
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-08-18 12:57:25 +0530

    counter-app-done-using-npx

---
+++ Counter-Application/counter-app/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  let [counter, setCounter] = React.useState(0);
+
+  const addValue = () => {
+    counter = counter + 1;
+    setCounter(counter);
+    console.log(counter);
+  };
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    } else {
+      setCounter(counter == 0);
+    }
+  };
+
+  return (
+    <>
+      <h1>First project using react</h1>
+      <h3>Counter App:{counter}</h3>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  );
+}
+
+export default App;
